b4cd7f006adef7bbc4925156552a22c7663f6d5c
Kevin Neyer <kevinneyer@gmail.com>
2020-04-04 18:39:03 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++
@@ -1,8 +0,0 @@
-# Write your solution here
-puts "You know what year it is??"
-this_year = Time.now.year 
-if this_year == 2019
-  puts "Hey, it's 2019!" 
-else 
-  puts"Hey, its #{this_year}!"
-end
